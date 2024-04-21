ef56ce72206fbd767acc502c42e92e867eee27ca
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:39:17 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // write code here
+<script>
+        let num1 = prompt("Enter the first number");
+        let num2 = prompt("Enter the second number");
+
+        
+        if (!isNaN(num1) && !isNaN(num2)) {
+        num1 = Number(num1);
+        num2 = Number(num2); 
+
+        let sum = num1 + num2;
+        alert("Sum: " + sum);
+        } else {
+        alert("Invalid number");
+        }
+
+</script>
